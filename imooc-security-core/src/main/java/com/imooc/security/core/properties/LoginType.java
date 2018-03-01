8daaf19b67fcc90bc0564ce6084f274b97aa022a
lihaoyang <945588456@qq.com>
2018-03-01 14:29:00 +0800

    登录类型枚举类，json或者重定向

---
+++ imooc-security-core/src/main/java/com/imooc/security/core/properties/LoginType.java
@@ -0,0 +1,21 @@
+package com.imooc.security.core.properties;
+
+/**
+ * 登录类型枚举
+ * ClassName: LoginType 
+ * @Description: 登录类型
+ * @author lihaoyang
+ * @date 2018年3月1日
+ */
+public enum LoginType {
+
+	/**
+	 * 返回json
+	 */
+	JSON,
+	
+	/**
+	 * 重定向
+	 */
+	REDIRECT
+}
