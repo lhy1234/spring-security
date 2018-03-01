b8d102968a5c85e217f19411c72b1c7ae36b88df
lihaoyang <945588456@qq.com>
2018-03-01 14:26:32 +0800

    浏览器配置项,对应application.properties里的自定义浏览器配置

--- imooc-security-core/src/main/java/com/imooc/security/core/properties/BrowserProperties.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/properties/BrowserProperties.java
@@ -9,14 +9,25 @@ package com.imooc.security.core.properties;
  */
 public class BrowserProperties {
 
-	private String loginPage = "/login.html"; //用户未配置默认登录页
+	//用户未配置默认登录页
+	private String loginPage = "/login.html"; 
 
+	private LoginType loginType = LoginType.JSON ;
+	
 	public String getLoginPage() {
 		return loginPage;
 	}
 
 	public void setLoginPage(String loginPage) {
 		this.loginPage = loginPage;
+	}
+
+	public LoginType getLoginType() {
+		return loginType;
+	}
+
+	public void setLoginType(LoginType loginType) {
+		this.loginType = loginType;
 	}
 	
 }
