00efb1692e18e1dba5caa63ba9c5f1a17dbbfd86
lihaoyang <945588456@qq.com>
2018-03-02 15:10:04 +0800

    修改从请求里读取验证码参数，传递给VerifyCode验证码生成类的构造器

--- imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ValidateCodeController.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/validate/code/ValidateCodeController.java
@@ -6,11 +6,15 @@ import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.connect.web.HttpSessionSessionStrategy;
 import org.springframework.social.connect.web.SessionStrategy;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
+
+import com.imooc.security.core.properties.SecurityProperties;
 
 /**
  * 验证码Control
@@ -23,6 +27,9 @@ import org.springframework.web.context.request.ServletWebRequest;
 public class ValidateCodeController {
 	
 	public static final String SESSION_KEY = "SESSION_KEY_IMAGE_CODE";	
+	
+	@Autowired
+	private SecurityProperties securityProperties;
 	
 	//获取session
 	private SessionStrategy sessionStrategy = new HttpSessionSessionStrategy();
@@ -37,8 +44,14 @@ public class ValidateCodeController {
 
 	
 	private ImageCode createImageCode(HttpServletRequest request, HttpServletResponse response) {
-		VerifyCode verifyCode = new VerifyCode();
-		return new ImageCode(verifyCode.getImage(),verifyCode.getText(),60);
+		//先从request里读取有没有长、宽、字符个数参数，有的话就用，没有用默认的
+		int width  = ServletRequestUtils.getIntParameter(request, "width",securityProperties.getCode().getImage().getWidth());
+    	
+    	int height = ServletRequestUtils.getIntParameter(request, "height",securityProperties.getCode().getImage().getHeight());
+    	
+    	int charLength = this.securityProperties.getCode().getImage().getLength();
+		VerifyCode verifyCode = new VerifyCode(width,height,charLength);
+		return new ImageCode(verifyCode.getImage(),verifyCode.getText(),this.securityProperties.getCode().getImage().getExpireIn());
 	}
 
 }
