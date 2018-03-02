14de3be0284eda7f47ffa50ac1e303c39b0b90f6
lihaoyang <945588456@qq.com>
2018-03-02 15:11:18 +0800

    图片验证码配置对应的类

---
+++ imooc-security-core/src/main/java/com/imooc/security/core/properties/ImageCodeProperties.java
@@ -0,0 +1,54 @@
+package com.imooc.security.core.properties;
+
+/**
+ * 图片验证码配置类
+ * ClassName: ImageCodeProperties 
+ * @Description: 图片验证码配置类
+ * @author lihaoyang
+ * @date 2018年3月2日
+ */
+public class ImageCodeProperties {
+
+	//图片宽
+	private int width = 67;
+	//图片高
+	private int height = 23;
+	//验证码字符个数
+	private int length = 4;
+	//过期时间
+	private int expireIn = 60;
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	public int getExpireIn() {
+		return expireIn;
+	}
+
+	public void setExpireIn(int expireIn) {
+		this.expireIn = expireIn;
+	}
+	
+	
+}
